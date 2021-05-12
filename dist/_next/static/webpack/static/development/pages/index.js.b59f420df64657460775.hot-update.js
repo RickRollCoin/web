1cfb381298a1071523ed64e17c8ba94c53a655b0
Kacper <kacper@swoopfunding.com>
2021-05-12 20:38:23 +0200

    CI/CD in place

---
+++ dist/_next/static/webpack/static/development/pages/index.js.b59f420df64657460775.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./src/assets/images/logo-white.png":
+/*!******************************************!*\
+  !*** ./src/assets/images/logo-white.png ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/logo-white-4b6002fef7bab77732e3d797148f6f3b.png";
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b59f420df64657460775.hot-update.js.map
